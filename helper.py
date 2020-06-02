f34f29c98c16d3506aec5cf4b0f6574a4309628c
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-06-02 13:41:00 +0530

    added products + greq

--- helper.py
+++ helper.py
@@ -1,22 +1,59 @@
-import requests
 from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 from datetime import datetime
 import csv
+from multithread import getSoup_list
+import time
+import grequests
+
+def getSoup_list(urls):
+    MAX_CONNECTIONS = 100
+    requests = []
+    for x in range(0,len(urls),MAX_CONNECTIONS):
+        rs = (grequests.get(u, stream=False) for u in urls[x:x+MAX_CONNECTIONS])
+        time.sleep(0.2)
+        requests.extend(grequests.map(rs))
+    soups = []
+    for request in requests:
+        html = request.content
+        soup = BeautifulSoup(html, "html.parser")
+        soups.append(soup)
+    return soups
 
 def getSoup(link):
-    req = requests.get(link)
+    urls = [link]
+    MAX_CONNECTIONS = 100
+    requests = []
+    for x in range(0,len(urls),MAX_CONNECTIONS):
+        rs = (grequests.get(u, stream=False) for u in urls[x:x+MAX_CONNECTIONS])
+        time.sleep(0.2)
+        requests.extend(grequests.map(rs))
+    req = requests[0]
     html = req.content
     soup = BeautifulSoup(html, "html.parser")
     return soup
 
 def download_img(visual, name):
-    req = requests.get(visual)
+    urls = [visual]
+    MAX_CONNECTIONS = 100
+    requests = []
+    for x in range(0,len(urls),MAX_CONNECTIONS):
+        rs = (grequests.get(u, stream=False) for u in urls[x:x+MAX_CONNECTIONS])
+        time.sleep(0.2)
+        requests.extend(grequests.map(rs))
+    req = requests[0]
     open("images/{}".format(name), "wb").write(req.content)
 
 def download_pdf(pdf_link, name):
-    req = requests.get(pdf_link)
+    urls = [pdf_link]
+    MAX_CONNECTIONS = 100
+    requests = []
+    for x in range(0,len(urls),MAX_CONNECTIONS):
+        rs = (grequests.get(u, stream=False) for u in urls[x:x+MAX_CONNECTIONS])
+        time.sleep(0.2)
+        requests.extend(grequests.map(rs))
+    req = requests[0]
     open("pdfs/{}".format(name), "wb").write(req.content)
 
 def tabulate(csvfile, matrix):
@@ -64,81 +101,82 @@ def get_product_links(link):
     result[key] = product_links
     return result
 
-def get_product_inf(cat, link):
+def get_product_inf(cat, links):
     result = []
-    soup = getSoup(link)
-    item_code = soup.find("span", {"itemprop":"name"}).text.strip()
-    cats = cat.split(":")
-    criteria = []
-    value = []
-    units = []
-    brand = ""
-    names = []
-    section = []
-    for i in range(0, 3):
+    soups = getSoup_list(links)
+    for index_link, soup in enumerate(soups):
+        item_code = soup.find("span", {"itemprop":"name"}).text.strip()
+        cats = cat.split(":")
+        criteria = []
+        value = []
+        units = []
+        brand = ""
+        names = []
+        section = []
+        for i in range(0, 3):
+            try:
+                table = soup.findAll("table",{"class":"product-features-table"})[i]
+                table_rows = table.findAll("tr")
+                for table_row in table_rows:
+                    table_row = table_row.findAll("td")
+                    if i==0:
+                        section.append("Product Definition")
+                    if i==1:
+                        section.append("Product Performance")
+                    if i==2:
+                        section.append("Product Dimensions")
+                    if not table_row[0].text == "Brand":
+                        criteria.append(table_row[0].text)
+                        ans = table_row[1].text.split()
+                        value.append(ans[0])
+                        try:
+                            units.append(ans[1])
+                        except:
+                            units.append("")
+                    else:
+                        brand = table_row[1].text
+            except Exception as e:
+                print(e)
+                pass
+
+
+
+        
+        visuals = soup.find("div", {"class": "accordion-content"}).findAll("a")
+        for index, visual in enumerate(visuals):
+            visual = visual['href']
+            name = "{}-{}-{}.jpg".format(item_code.strip(), brand.strip(), index+1)
+            name = name.replace("/", "-")
+            names.append(name)
+            download_img(visual, name)
         try:
-            table = soup.findAll("table",{"class":"product-features-table"})[i]
-            table_rows = table.findAll("tr")
-            for table_row in table_rows:
-                table_row = table_row.findAll("td")
-                if i==0:
-                    section.append("Product Definition")
-                if i==1:
-                    section.append("Product Performance")
-                if i==2:
-                    section.append("Product Dimensions")
-                if not table_row[0].text == "Brand":
-                    criteria.append(table_row[0].text)
-                    ans = table_row[1].text.split()
-                    value.append(ans[0])
-                    try:
-                        units.append(ans[1])
-                    except:
-                        units.append("")
-                else:
-                    brand = table_row[1].text
-        except Exception as e:
-            print(e)
-            pass
-
-
-
-    
-    visuals = soup.find("div", {"class": "accordion-content"}).findAll("a")
-    for index, visual in enumerate(visuals):
-        visual = visual['href']
-        name = "{}-{}-{}.jpg".format(item_code.strip(), brand.strip(), index+1)
-        name = name.replace("/", "-")
-        names.append(name)
-        download_img(visual, name)
-    try:
-        pdf_link = soup.find("a", {"title": "Technical data"})['href']
-    except:
-        pdf_link = ""
-    try:
-        pdf_name = "{}-{}-{}.pdf".format(item_code.strip(), brand.strip(), "Datasheet")
-        download_pdf(pdf_link, pdf_name)
-    except:
-        pdf_name = ""
-    try:
-        cad = soup.find("a", {"title": "View CAD drawing"})['href']
-    except:
-        cad = ""
-    for index, c in enumerate(criteria):
-        row = []
-        row.append(item_code)
-        row.append(c)
-        row.append(value[index])
-        row.append(units[index])
-        row.append(brand)
-        row.append(section[index])
-        row.extend(names)
-        row.append(cad)
-        row.append(pdf_link)
-        row.append(pdf_name)
-        row.append(link)
-        row.extend(cats)
-        result.append(row)
+            pdf_link = soup.find("a", {"title": "Technical data"})['href']
+        except:
+            pdf_link = ""
+        try:
+            pdf_name = "{}-{}-{}.pdf".format(item_code.strip(), brand.strip(), "Datasheet")
+            download_pdf(pdf_link, pdf_name)
+        except:
+            pdf_name = ""
+        try:
+            cad = soup.find("a", {"title": "View CAD drawing"})['href']
+        except:
+            cad = ""
+        for index, c in enumerate(criteria):
+            row = []
+            row.append(item_code)
+            row.append(c)
+            row.append(value[index])
+            row.append(units[index])
+            row.append(brand)
+            row.append(section[index])
+            row.extend(names)
+            row.append(cad)
+            row.append(pdf_link)
+            row.append(pdf_name)
+            row.append(links[index_link])
+            row.extend(cats)
+            result.append(row)
     return result
 
 def make_sheet():
@@ -147,14 +185,13 @@ def make_sheet():
     heading = [["Item_Code","Criteria",	"Value","Units","Brand","Section","Product Image","Product Diagram","Product Diagram 1","CAD Link","PDF Link","PDF File","Direct Web Link",	"Category 1","Category 2","Category 3","Category 4","Category 5","Category 6"]]
     tabulate("results.csv", heading)
     for cat, links in products.items():
-        for link in links:
-            try:
-                print(link)
-                result = get_product_inf(cat, link)
-                tabulate("results.csv",result)
-            except Exception as e:
-                print(e)
-                pass
+        try:
+            print(cat)
+            result = get_product_inf(cat, links)
+            tabulate("results.csv",result)
+        except Exception as e:
+            print(e)
+            pass
 
 def get_category_driver():
     link = "https://eshop.ntn-snr.com/en/Industry-Solutions-12078.html"
@@ -179,6 +216,6 @@ def get_links_driver():
 def driver():
     # get_category_driver()
     # get_product_links("https://eshop.ntn-snr.com/en/Single-row-deep-groove-ball-bearings-2246325.html")
-    get_links_driver()
-    # get_product_inf("Home e-Shop:Industry Solutions:Ball bearings:Radial ball bearings:Single row deep groove ball bearings","https://eshop.ntn-snr.com/en/16003-16C3-689756.html")
+    # get_links_driver()
+    # get_product_inf("Home e-Shop:Industry Solutions:Ball bearings:Radial ball bearings:Single row deep groove ball bearings",["https://eshop.ntn-snr.com/en/16003-16C3-689756.html"])
     make_sheet()
